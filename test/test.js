bf9f42e567480ae3aade9d378f7bf1c1857285fa
phette23 <phette23@gmail.com>
2016-09-27 22:01:09 -0700

    add 2 more test sets

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var parse = require('../src/index')
 // tests use actual issue strings from library holdings
 
 // https://library.cca.edu/cgi-bin/koha/opac-detail.pl?biblionumber=7978
-exports['"Archetype." string'] = function (test) {
+exports['"Archetype."'] = function (test) {
     test.deepEqual(
         parse('v.1 no.3 Aut 1979'),
         {'issue': 3, 'original_string': 'v.1 no.3 Aut 1979', 'volume': 1, 'year': 1979}
@@ -15,6 +15,40 @@ exports['"Archetype." string'] = function (test) {
     test.deepEqual(
         parse('v.3 no.1 Spr 1982'),
         {'issue': 1, 'original_string': 'v.3 no.1 Spr 1982', 'volume': 3, 'year': 1982}
+    )
+    test.done()
+}
+
+// https://library.cca.edu/cgi-bin/koha/opac-detail.pl?biblionumber=9530
+exports['"Octavo"'] = function (test) {
+    test.deepEqual(
+        parse('v.88.5'),
+        // faced with this string, I think not assuming the ".5" is an issue is correct behavior
+        {'issue': null, 'original_string': 'v.88.5', 'volume': 88, 'year': null}
+    )
+    test.deepEqual(
+        parse('v.90.7'),
+        {'issue': null, 'original_string': 'v.90.7', 'volume': 90, 'year': null}
+    )
+    test.done()
+}
+
+// https://library.cca.edu/cgi-bin/koha/opac-detail.pl?biblionumber=34565
+exports['"Grey room."'] = function (test) {
+    test.deepEqual(
+        parse('no.63 Spr 2016'),
+        // faced with this string, I think not assuming the ".5" is an issue is correct behavior
+        {'issue': 63, 'original_string': 'no.63 Spr 2016', 'volume': null, 'year': 2016}
+    )
+    test.deepEqual(
+        parse('no.10-13 2003'),
+        // @TODO brings up a question: how to handle issue ranges?
+        {'issue': 10, 'original_string': 'no.10-13 2003', 'volume': null, 'year': 2003}
+    )
+    test.deepEqual(
+        parse('no.5-9 2002'),
+        // @TODO brings up a question: how to handle issue ranges?
+        {'issue': 5, 'original_string': 'no.5-9 2002', 'volume': null, 'year': 2002}
     )
     test.done()
 }
